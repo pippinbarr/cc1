038ccf913506148a34e9563ec62d69842996bbd6
Pippin Barr <pippin.barr@gmail.com>
2021-09-28 14:58:19 -0400

    Live: animating the appearance of 10 circles

--- b/experiments/conditionals-experiments/js/script.js
+++ b/experiments/conditionals-experiments/js/script.js
@@ -2,65 +2,34 @@
 
 /**
 
-* Multiple images (happy face/scared face)
+* √ Multiple images (happy face/scared face)
 * for-loops (discomfort with, why are they instant?)
 * delaying code
 * pushing and popping
 
 */
 
-let normalFace = undefined;
-let scaredFace = undefined;
-let lookingRightFace = undefined;
-let lookingLeftFace = undefined;
-
-let face = {
-  x: 250,
-  y: 250,
-  terrorThreshold: 200,
-  image: undefined
-}
-
-function preload() {
-  normalFace = loadImage(`assets/images/normal.png`);
-  scaredFace = loadImage(`assets/images/scared.png`);
-  lookingRightFace = loadImage(`assets/images/look-right.png`);
-  lookingLeftFace = loadImage(`assets/images/look-left.png`);
-}
+let numCircles = 0;
 
 function setup() {
   createCanvas(500, 500);
-
-  face.image = lookingRightFace;
+  background(0);
+  frameRate(10);
 }
 
 function draw() {
-  background(0);
 
-  imageMode(CENTER);
-
-  // Calculate the distance between the mouse and the face
-  let d = dist(mouseX, mouseY, face.x, face.y);
-  // Check if the mouse is TOO CLOSE!!!
-  if (d < face.terrorThreshold) {
-    // Scared face
-    face.image = scaredFace;
-  } else {
-    // Not scared face
-    if (mouseX < face.x) {
-      // Mouse is to the left, so look left
-      face.image = lookingLeftFace;
-    } else {
-      // Mouse is to the right, so look right
-      face.image = lookingRightFace;
-    }
+  let x = 20;
+  let y = height / 2;
+  let size = 40;
+  // Count from 0 up to numCircles
+  for (let i = 0; i < numCircles; i++) {
+    noFill();
+    stroke(255);
+    ellipse(x + i * size / 1.25, y, size);
   }
-  // Display the chosen image
-  image(face.image, face.x, face.y);
 
-  // noFill();
-  // stroke(255, 0, 0);
-  // ellipse(face.x, face.y, face.terrorThreshold * 2);
+  numCircles = constrain(numCircles + 1, 0, 10);
 }
 
 
