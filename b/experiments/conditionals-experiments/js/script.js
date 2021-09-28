426626589445fc57ade34f2f9abc40d992ab8e22
Pippin Barr <pippin.barr@gmail.com>
2021-09-28 14:01:39 -0400

    Live: loaded two images, display scared face

--- b/experiments/conditionals-experiments/js/script.js
+++ b/experiments/conditionals-experiments/js/script.js
@@ -9,12 +9,23 @@
 
 */
 
+let normalFace = undefined;
+let scaredFace = undefined;
+
+function preload() {
+  normalFace = loadImage(`assets/images/normal.png`);
+  scaredFace = loadImage(`assets/images/scared.png`);
+}
+
 function setup() {
   createCanvas(500, 500);
 }
 
 function draw() {
   background(0);
+
+  imageMode(CENTER);
+  image(scaredFace, width / 2, height / 2);
 }
 
 
