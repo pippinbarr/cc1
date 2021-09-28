96011ee943f0e158334654f16ba4d9abbeb48599
Pippin Barr <pippin.barr@gmail.com>
2021-09-28 13:55:46 -0400

    Live: wrote to-do list for session

--- b/experiments/conditionals-experiments/js/script.js
+++ b/experiments/conditionals-experiments/js/script.js
@@ -1,5 +1,14 @@
 "use strict";
 
+/**
+
+* Multiple images (happy face/scared face)
+* for-loops (discomfort with, why are they instant?)
+* delaying code
+* pushing and popping
+
+*/
+
 function setup() {
   createCanvas(500, 500);
 }
